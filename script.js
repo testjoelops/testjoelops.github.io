94fc9d1618134382a8b65afaceef5750a7daf9aa
testjoelops <testjoelops@outlook.com>
2026-08-02 16:27:39 +0530

    Update script.js
    window.onload
    formdata logger
    hidden field value logger

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 
-console.log("Staarust version 01")
+console.log("Staarust")
 
-const form = document.getElementById("newsletter-form");
-const emailInput = document.getElementById("email");
+window.addEventListener("load", function () {
+    window.scrollTo(0, 0);
+});
+
+const newsletter = document.getElementById("newsletter-form");
+const email = document.getElementById("email");
+const firstName = document.getElementById("first_name");
 const thankYou = document.getElementById("thank-you");
+const form = document.getElementById("sd-newsletter");
 
-console.log(form);
+
+email.addEventListener("input", function () {
+	const username = email.value.split("@")[0];
+	firstName.value = username;
+});
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-    console.log(emailInput.value);
-    form.style.display = "none";
+
+	const formData = new FormData(form);
+	const data = Object.fromEntries(formData.entries());
+
+	console.log(data);
+
+    newsletter.style.display = "none";
     thankYou.style.display = "block";
 
 });
-
